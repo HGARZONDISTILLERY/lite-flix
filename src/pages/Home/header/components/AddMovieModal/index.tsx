c6165453028522d2654424bd71ceed1df16afe3d
Hernan Garzon <hernan.garzon@distillery.com>
2024-06-23 20:42:03 -0300

    Work on modal confirmation flow and fix bug

--- src/pages/Home/header/components/AddMovieModal/index.tsx
+++ src/pages/Home/header/components/AddMovieModal/index.tsx
@@ -1,7 +1,7 @@
 import './styles.css'
 
 import { useEffect, useState, useRef, FC } from 'react'
-import {ref, uploadBytes, listAll, getDownloadURL } from 'firebase/storage'
+import {ref, getDownloadURL, uploadBytesResumable } from 'firebase/storage'
 import { addDoc, collection } from 'firebase/firestore'
 
 import { DropzoneArea } from "mui-file-dropzone"
@@ -16,8 +16,10 @@ import CloseIcon from '@mui/icons-material/Close'
 import { storage, firestore } from '../../../../../firebase'
 import { MyMovieElement } from '../../../../../utils/types'
 import Clip from '../../../../../assets/icons/Clip'
+import Logo from '../Logo'
+import { BarLoader } from 'react-spinners'
 
-// Styled component example
+// Styled component examples
 const CustomTextField = styled(TextField)({
   '& label': {
     color: '#fff',
@@ -48,6 +50,21 @@ const CustomTextField = styled(TextField)({
   }
 })
 
+const MovieModalButton = styled(Button)({
+  width: '248px',
+  height: '56px',
+  backgroundColor: '#fff',
+  opacity: '0.3',
+  borderRadius: '0',
+  color: '#242424',
+  marginTop: '48px',
+  fontSize: '18px',
+  '&:hover': {
+    backgroundColor: '#fff',
+    opacity: '0.6',
+  },
+})
+
 const containerMovieModalStyle = {
   position: 'absolute' as 'absolute',
   top: '50%',
@@ -60,7 +77,6 @@ const containerMovieModalStyle = {
   textAlign: 'center',
 };
 
-const imageListRef = ref(storage, "images/")
 const firebaseDbRef = collection(firestore, "movies")
 
 interface AddMovieProps {
@@ -70,64 +86,161 @@ interface AddMovieProps {
 const AddMovieModal = ({ AddMovieButton }: AddMovieProps) => {
   const [open, setOpen] = useState(false)
   const handleOpen = () => setOpen(true)
-  const handleClose = () => setOpen(false)
+  const handleClose = () => {
+    setOpen(false)
+    setMovieElementIsLoading(false)
+    setMovieElementUploaded(false)
+  }
   const [movieFileData, setMovieFile] = useState<any>()
-  const [imageList, setImageList] = useState<string[]>([])
   const [movieElement, setMovieElement] = useState<MyMovieElement>({})
+  const [movieElementIsLoading, setMovieElementIsLoading] = useState(false)
+  const [movieElementUploaded, setMovieElementUploaded] = useState(false)
+  const [uploadProgress, setUploadProgress] = useState(0)
 
   const titleRef = useRef<HTMLInputElement>(null)
 
   const handleSave = async (e: any) => {
     e.preventDefault();
+    let movieTitle = ''
+    if (titleRef?.current) {
+      movieTitle = String(titleRef?.current.value)
+    }
+    setMovieElementIsLoading(true)
   
     if (movieFileData === null || movieFileData.length === 0) return
   
     const file = movieFileData[0]
     const imageRef = ref(storage, `images/${file.name}`)
-    
-    uploadBytes(imageRef, file).then((snapshot) => {
-      getDownloadURL(snapshot.ref).then(url => {
-        setImageList(prev => [...prev, url])
-        if (titleRef.current !== null) {
-          setMovieElement({title: String(titleRef.current.value), image: url})
+    const uploadTask = uploadBytesResumable(imageRef, file);
+
+    uploadTask.on('state_changed', 
+      (snapshot) => {
+        const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        setUploadProgress(progress)
+
+        switch (snapshot.state) {
+          case 'paused':
+            console.log('Carga pausada');
+            break;
+          case 'running':
+            console.log('Carga en progreso');
+            setMovieElementIsLoading(true);
+            setMovieElementUploaded(false);
+            break;
         }
-      })
-    }).catch((error) => {
-      console.error('Error uploading file:', error)
-    })
+      }, 
+      (error) => {
+        console.error('Error uploading file:', error);
+        setMovieElementIsLoading(false);
+      }, 
+      () => {
+        console.log('Carga completada con éxito');
+        setMovieElementIsLoading(false);
+        setMovieElementUploaded(true);
+        getDownloadURL(uploadTask.snapshot.ref).then((url) => {
+          setMovieElement({ title: movieTitle, image: url });
+        }).catch((error) => {
+          console.error('Error getting download URL:', error);
+          setMovieElementIsLoading(false);
+        });
+      }
+    );
+  }
+
+  const goToHome = () => {
+    location.reload()
   }
 
   useEffect(() => {
-    listAll(imageListRef).then(response => {
-      response.items.forEach(item => {
-        getDownloadURL(item).then((url) => {
-          setImageList(prev => [...prev, url])
-        }).catch((error) => {
-          console.error('Error getting download URL:', error)
-        })
-      })
-    }).catch((error) => {
-      console.error('Error listing images:', error)
-    })
-  }, [])
-
-  useEffect(() => {
-    if (titleRef.current !== null) {
+    if (
+      movieElement && movieElement.title &&
+      movieElement.title.trim() !== '' &&
+      movieElement.image &&
+      movieElement.image.trim() !== ''
+      ) {
       try {
         addDoc(firebaseDbRef, movieElement)
       } catch(e) {
         console.error(e)
       }
     }
-  }, [titleRef, firebaseDbRef, movieElement])
+  }, [movieElement])
 
   useEffect(() => {
-    const curatedImageList = [...new Set(imageList)]
-  }, [imageList])
+    console.log('movieElement', movieElement)
+  }, [movieElement])
+
+  const addMovieContentForm = (
+    <>
+      <Typography id="movie-modal-title" sx={{ color: '#64EEBC', fontSize: '20px', fontWeight: '700' }}>
+        Agregar película
+      </Typography>
+      <form onSubmit={handleSave}>
+        <DropzoneArea
+          onChange={(event) => setMovieFile(event)}
+          dropzoneText='Agregá un archivo o arrastralo y soltalo aquí'
+          dropzoneClass='dropzone'
+          Icon={Clip}
+          filesLimit={1}
+          showAlerts={false}
+          showFileNames
+          />
+        <CustomTextField
+          id="movie-title"
+          label="Titulo"
+          variant="standard"
+          sx={{
+            textAlign: 'center',
+            width: '248px',
+            marginTop: '48px',
+          }}
+          inputRef={titleRef}
+          type='text'
+          required
+        />
+        <Box>
+          <MovieModalButton type="submit" sx={{background: '#fff', opacity: .9}}>
+            Subir película
+          </MovieModalButton>
+        </Box>
+      </form>
+    </>
+  )
+
+  const confirmationContentForm = (
+    <>
+      <Logo />
+      <form>
+        <Typography sx={{ marginTop: '72px', color: '#fff', fontWeight: '700', fontSize: '24px' }}>
+          ¡Felicitaciones!
+        </Typography>
+        <Typography sx={{ color: '#fff', fontSize: '20px' }}>
+          Liteflix The movie fue correctamente subida.
+        </Typography>
+        <MovieModalButton onClick={goToHome}>Ir a Home</MovieModalButton>
+      </form>
+    </>
+  )
+
+  const loadingMovieContentForm = (
+    <>
+      <Typography id="movie-modal-title" sx={{ color: '#64EEBC', fontSize: '20px', fontWeight: '700' }}>
+        Agregar película
+      </Typography>
+      <Box sx={{ width: '100%', margin: '60px 0', textAlign: 'center' }}>
+        <Typography>Cargando {uploadProgress}</Typography>
+        <BarLoader color="#36d7b7" width={600} />
+      </Box>
+      <Box>
+        <MovieModalButton type="submit" disabled>
+          Subir película
+        </MovieModalButton>
+      </Box>
+    </>
+  )
 
   return (
     <div>
-      {/* <Button sx={{color: '#fff', fontSize: '18px'}} onClick={handleOpen}>+ <strong>Agregar película</strong></Button> */}
       <AddMovieButton onClick={handleOpen} />
       <Modal
         open={open}
@@ -144,54 +257,9 @@ const AddMovieModal = ({ AddMovieButton }: AddMovieProps) => {
           <Box sx={{width: '100%', textAlign: 'right', color: '#fff'}}>
             <CloseIcon onClick={handleClose} sx={{cursor: 'pointer'}} />
           </Box>
-          <Typography id="movie-modal-title" sx={{ color: '#64EEBC', fontSize: '20px', fontWeight: '700' }}>
-            Agregar película
-          </Typography>
-          <form onSubmit={handleSave}>
-            <DropzoneArea
-              onChange={(event) => setMovieFile(event)}
-              dropzoneText='Agregá un archivo o arrastralo y soltalo aquí'
-              dropzoneClass='dropzone'
-              Icon={Clip}
-              filesLimit={1}
-              showAlerts={false}
-              showFileNames
-              />
-            <CustomTextField
-              id="movie-title"
-              label="Titulo"
-              variant="standard"
-              sx={{
-                textAlign: 'center',
-                width: '248px',
-                marginTop: '48px',
-              }}
-              inputRef={titleRef}
-              type='text'
-              required
-            />
-            <Box>
-              <Button 
-              type="submit" 
-              sx={{
-                width: '248px',
-                height: '56px',
-                backgroundColor: '#fff',
-                opacity: '0.3',
-                borderRadius: '0',
-                color: '#242424',
-                marginTop: '48px',
-                fontSize: '18px',
-                '&:hover': {
-                  backgroundColor: '#fff',
-                  opacity: '0.6',
-                }
-                }}
-              >
-                Subir película
-              </Button>
-            </Box>
-          </form>
+          { movieElementIsLoading && loadingMovieContentForm }
+          { movieElementUploaded && confirmationContentForm }
+          { !movieElementIsLoading && !movieElementUploaded && addMovieContentForm }
         </Box>
       </Modal>
     </div>
